7f1ef0ee2f8936d82d3023104ad544738c4d5728
Kevin Guo <kevinsunny2008@gmail.com>
2020-10-30 01:44:48 +0800

    Use observer pattern for sign out process temporary

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import { SelectCurrentUser } from "./redux/user/user.selector";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 import Header from "./components/header/header.component";
@@ -11,28 +12,32 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import {signInSuccess} from './redux/user/user.action'
 
 class App extends React.Component {
-  componentDidMount() {
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
+  unsubscribeFromAuth = null;
 
-    //     userRef.onSnapshot((snapshot) => {
-    //       setCurrentUser({
-    //         id: snapshot.id,
-    //         ...snapshot.data(),
-    //       });
-    //     });
-    //   } else {
-    //     setCurrentUser(userAuth);
-    //     //Only execute once to add collections to firebase
-    //     // addCollectionsAndDocuments(
-    //     //   "collections",
-    //     //   collectionsArray.map(({ title, items }) => ({ title, items }))
-    //     // );
-    //   }
-    // });
+  componentDidMount() {
+    const {signInSuccess} = this.props;    
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          signInSuccess({
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+      } else {
+        signInSuccess(userAuth);
+        //Only execute once to add collections to firebase
+        // addCollectionsAndDocuments(
+        //   "collections",
+        //   collectionsArray.map(({ title, items }) => ({ title, items }))
+        // );
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -68,5 +73,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: SelectCurrentUser,
 });
 
+const mapDispatchToProps = dispatch => ({
+  signInSuccess: (user) => dispatch(signInSuccess(user))
+})
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
